a18cba50d0ad660c730cf504c2088d7715dc564f
baptiste3672 <baptiste.mergey@gmx.fr>
2021-09-29 23:19:58 +0200

    Ajout de la fonction Factoriel

--- Perimetre/src/fr/baptiste/perimetre/fontions.java
+++ Perimetre/src/fr/baptiste/perimetre/fontions.java
@@ -1,15 +1,22 @@
 package fr.baptiste.perimetre;
 
+import java.util.Scanner;
+
 public class fontions {
 	
 	public static void main(String [] parametre)
 	{
 		int a, compteur;
+		Scanner sc = new Scanner(System.in);
 		for(compteur = -5; compteur <= 5; compteur ++)
 		{
 			a = calculer(compteur);
 			System.out.println(a);
 		}
+		System.out.println("Donnez le nombre factoriel : ");
+		int nFactoriel = sc.nextInt();
+		nFactoriel= factoriel(nFactoriel);
+		System.out.println("Valeur de la factoriel : " + nFactoriel);
 	}
 	
 	public static int calculer (int x)
@@ -17,5 +24,15 @@ public class fontions {
 		int resultat;
 		resultat = -x * x + 3 * x - 2;
 		return resultat;
+	}
+	
+	public static int factoriel(int x)
+	{
+		int calcul = x;
+		for(int i = x-1; i > 0; i--)
+		{
+			calcul = calcul * i;
+		}
+		return calcul;
 	}
 }
